a89c5b7526ba16ef340c2fb6f6c4a13d9bbac662
LudaNjubara <mihael.sestak00@gmail.com>
2023-02-01 19:16:51 +0100

    Increased security of the application by removing hardcoded properties

--- config/firebaseConfig.js
+++ config/firebaseConfig.js
@@ -1,19 +1,18 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth, GithubAuthProvider, GoogleAuthProvider } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
+// Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBdQ9gfc1xlL_GZ8OMccukJOUn0wEhnFzc",
-  authDomain: "imager-f9e88.firebaseapp.com",
-  projectId: "imager-f9e88",
-  storageBucket: "imager-f9e88.appspot.com",
-  messagingSenderId: "770174664597",
-  appId: "1:770174664597:web:85eec371cd6baf7a51c09c",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
+
+console.log(firebaseConfig);
 
 // Initialize Firebase
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
