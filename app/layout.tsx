81b88d6796ba4906a89eae37c4d3623c898eba0d
LudaNjubara <mihael.sestak00@gmail.com>
2023-03-11 12:15:28 +0100

    Added Vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useEffect } from "react";
+import { Analytics } from "@vercel/analytics/react";
 import { useRouter, usePathname } from "next/navigation";
 import { Provider, useDispatch } from "react-redux";
 import { useAuthState } from "react-firebase-hooks/auth";
@@ -21,74 +22,75 @@ import facade from "../services/facade.class";
 import "../styles/globals.css";
 
 function RootLayoutInner({ children }: { children: React.ReactNode }) {
-  const [user, loading, error] = useAuthState(auth);
-  const dispatch = useDispatch();
-  const router = useRouter();
-  const pathname = usePathname();
-  const reduxUser = useAppSelector((state) => state.user);
-  const { userData } = useUserData(reduxUser.uid);
+	const [user, loading, error] = useAuthState(auth);
+	const dispatch = useDispatch();
+	const router = useRouter();
+	const pathname = usePathname();
+	const reduxUser = useAppSelector((state) => state.user);
+	const { userData } = useUserData(reduxUser.uid);
 
-  useEffect(() => {
-    if (!loading && !user) {
-      if (pathname !== "/login" && pathname !== "/register") router.push("/login");
-      dispatch(logout());
-    } else if (!loading && user) {
-      dispatch(
-        login({
-          uid: user.uid,
-          isAnonymous: user.isAnonymous,
-          email: user.email,
-          displayName:
-            userData?.username ??
-            user.displayName ??
-            user.providerData[0]?.displayName ??
-            user.providerData[1]?.displayName,
-          photoURL: user?.photoURL ?? user.providerData[0]?.photoURL ?? undefined,
-          emailVerified: user.emailVerified,
-          phoneNumber: user.phoneNumber,
-          providerId: user.providerId,
-          metadata: {
-            creationTime: user.metadata.creationTime,
-            lastSignInTime: user.metadata.lastSignInTime,
-          },
-        })
-      );
+	useEffect(() => {
+		if (!loading && !user) {
+			if (pathname !== "/login" && pathname !== "/register") router.push("/login");
+			dispatch(logout());
+		} else if (!loading && user) {
+			dispatch(
+				login({
+					uid: user.uid,
+					isAnonymous: user.isAnonymous,
+					email: user.email,
+					displayName:
+						userData?.username ??
+						user.displayName ??
+						user.providerData[0]?.displayName ??
+						user.providerData[1]?.displayName,
+					photoURL: user?.photoURL ?? user.providerData[0]?.photoURL ?? undefined,
+					emailVerified: user.emailVerified,
+					phoneNumber: user.phoneNumber,
+					providerId: user.providerId,
+					metadata: {
+						creationTime: user.metadata.creationTime,
+						lastSignInTime: user.metadata.lastSignInTime,
+					},
+				})
+			);
 
-      if (pathname === "/login" || pathname === "/register") router.push("/");
-    }
-  }, [user, loading, error]);
+			if (pathname === "/login" || pathname === "/register") router.push("/");
+		}
+	}, [user, loading, error]);
 
-  useEffect(() => {
-    if (user && !!userData) {
-      try {
-        facade.UpdateUserAccountPlan(user.uid, userData, {
-          fromProfilePage: false,
-        });
-      } catch (error) {
-        console.error(error);
-      }
-    }
-  }, [user, userData]);
+	useEffect(() => {
+		if (user && !!userData) {
+			try {
+				facade.UpdateUserAccountPlan(user.uid, userData, {
+					fromProfilePage: false,
+				});
+			} catch (error) {
+				console.error(error);
+			}
+		}
+	}, [user, userData]);
 
-  return (
-    <div id="pageWrapper">
-      <Sidebar />
-      {user && !user.isAnonymous && <ImageUpload />}
-      {!loading && <main id="main">{children}</main>}
-    </div>
-  );
+	return (
+		<div id="pageWrapper">
+			<Sidebar />
+			{user && !user.isAnonymous && <ImageUpload />}
+			{!loading && <main id="main">{children}</main>}
+		</div>
+	);
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="en">
-      <Head />
+	return (
+		<html lang="en">
+			<Head />
 
-      <body className={poppins.className}>
-        <Provider store={store}>
-          <RootLayoutInner children={children} />
-        </Provider>
-      </body>
-    </html>
-  );
+			<body className={poppins.className}>
+				<Provider store={store}>
+					<RootLayoutInner children={children} />
+				</Provider>
+				<Analytics />
+			</body>
+		</html>
+	);
 }
