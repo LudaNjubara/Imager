4af007c0e4227d730d4d841167451d12bf8b39ab
LudaNjubara <mihael.sestak00@gmail.com>
2023-05-02 17:08:29 +0200

    Setup Cypress

---
+++ cypress.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  projectId: "2ec8id",
+
+
+  e2e: {
+    baseUrl: "http://localhost:3000",
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    supportFile: false
+  },
+
+  component: {
+    devServer: {
+      framework: "next",
+      bundler: "webpack",
+    },
+  },
+});
