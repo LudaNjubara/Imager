b8a64807a30cb194bf075199ce5135dcac9fbec8
LudaNjubara <mihael.sestak00@gmail.com>
2023-06-18 03:04:26 +0200

    changed baseUrl

--- cypress.config.ts
+++ cypress.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
 
 
   e2e: {
-    baseUrl: "http://localhost:3000",
+    baseUrl: "http://localhost:3000/",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
